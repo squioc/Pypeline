c3142f073a1db92ce3a3c4559da5f07632974236
Quioc Sébastien <sebastien.quioc@yahoo.com>
2012-01-14 00:58:35 +0100

    add iteration support on the reader's data

--- pypeline/core.py
+++ pypeline/core.py
@@ -1,16 +1,25 @@
 # -*- coding: UTF8 -*-
 
+from collections import Iterable
+
 class Pypeline(object):
     def __init__(self, reader, writer, transformers = list()):
         self.reader = reader
         self.writer = writer
         self.transformers = transformers
 
+    def __get_iter(self, obj):
+        if isinstance(obj, Iterable):
+            for element in obj:
+                yield element
+        else:
+            yield obj
+
     def __call__(self):
-        content = self.reader()
-        for transformer in self.transformers:
-            content = transformer(content)
-        self.writer(content)
+        for content in self.__get_iter(self.reader()):
+            for transformer in self.transformers:
+                content = transformer(content)
+            self.writer(content)
 
 class Source(object):
     pass
